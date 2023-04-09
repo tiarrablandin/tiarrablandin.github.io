929d9660e317f7df9782d009b3dc837c7f51c52d
tblandin <tiarra.blandin@outlook.com>
2023-04-09 14:03:46 -0600

    done for now

--- ngPortfolio/src/app/components/navbar/navbar.component.ts
+++ ngPortfolio/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
+
+  openPdf() {
+  window.open("assets/TiarraBlandinResume.pdf");
+  }
 }
